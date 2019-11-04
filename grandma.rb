35542eb1d6ae4d2f3427e790127525d7248627ee
Henry Phan <h.phan06@gmail.com>
2019-11-04 21:41:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else phrase == phrase.downcase
-    "HUH?!SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
