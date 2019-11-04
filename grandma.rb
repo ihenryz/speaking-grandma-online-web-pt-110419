28a414397c573c0902178d85445fa2b7e4471f19
Henry Phan <h.phan06@gmail.com>
2019-11-04 22:09:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -22,7 +22,7 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
   elsif phrase == phrase
     "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase
+  else phrase == phrase.captialize
     "NO, NOT SINCE 1938!"
   end
 end
