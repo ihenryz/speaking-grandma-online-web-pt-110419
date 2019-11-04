c4d7688b9a256c646111788635205d00e303c624
Henry Phan <h.phan06@gmail.com>
2019-11-04 22:07:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.downcape
+  elsif phrase == phrase.downcase
     "NO, NOT SINCE 1938!"
   elsif phrase == phrase
     "HUH?! SPEAK UP, SONNY!"
