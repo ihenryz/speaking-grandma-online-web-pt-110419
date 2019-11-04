3b2b98e10d700a982428af8695832132a6cdeb24
Henry Phan <h.phan06@gmail.com>
2019-11-04 22:14:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,8 +20,8 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
   elsif phrase == phrase.downcase
     "NO, NOT SINCE 1938!"
-  elsif phrase == phrase
-    "HUH?! SPEAK UP, SONNY!"
+  # elsif phrase == phrase
+  #   "HUH?! SPEAK UP, SONNY!"
   else phrase == "WHAT?"  
     "NO, NOT SINCE 1938!"
   end
