a07b8c73139154054a894630c741c7ea89b41f00
Henry Phan <h.phan06@gmail.com>
2019-11-04 21:33:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma("")
-  if "" == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
